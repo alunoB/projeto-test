dd12789a8ec16193e25a2240ec62d5cb5715f1c6
alunoB <144171238+alunoB@users.noreply.github.com>
2023-11-21 16:18:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function spanElement() {
+  console.log("Este é um elemento SPAN");
+}
+
+function divElement() {
+  console.log("Este é um elemento DIV");
+  document.getElementById("audio").play();
+}
